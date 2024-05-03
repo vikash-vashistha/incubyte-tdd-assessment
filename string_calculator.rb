24bf332446f7c57a6a66dbaf6434152f9c939ac7
vikash-vashistha <vikashvashitha.dev@gmail.com>
2024-05-03 09:41:06 +0530

    Refactor StringCalculator#add method to handle custom delimiters and negative numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,5 +1,27 @@
 class StringCalculator
   def add(numbers)
-    # Implementation goes here
+    return 0 if numbers.empty?
+
+    delimiter = ","
+    if numbers.start_with?("//")
+      delimiter, numbers = parse_custom_delimiter(numbers)
+    end
+
+    numbers = numbers.tr("\n", delimiter)
+    nums = numbers.split(delimiter).map(&:to_i)
+
+    negatives = nums.select { |num| num < 0 }
+    if negatives.any?
+      raise "negative numbers not allowed #{negatives.join(',')}"
+    end
+
+    nums.sum
+  end
+
+  private
+
+  def parse_custom_delimiter(numbers)
+    delimiter, numbers = numbers.split("\n", 2)
+    [delimiter[2..], numbers]
   end
 end
